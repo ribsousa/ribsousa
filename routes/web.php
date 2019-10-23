d6144eeed0ea87a2124d3318676ca7c8876b978e
ribsousa <ribbsousa@gmail.com>
2019-10-22 22:43:03 -0300

    Fix bug route

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*
 Route::get('/', function () {
     return view('welcome');
 });
-*/
-Route::get('/{any}', function () {
-    return view('welcome');
-})->where('any', '.*');
